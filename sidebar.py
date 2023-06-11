6f2a3813165f854665e7f96a84e3cb465d2dbcc0
Hristo Goumnerov <goumnerov@gmail.com>
2023-06-11 14:49:04 +0200

    new requirements

--- sidebar.py
+++ sidebar.py
@@ -1,16 +1,17 @@
 import streamlit as st
 
 
-def make_sidebar(movie_names, item_ids, user_ids):
+def make_sidebar(names, movie_names, item_ids, user_ids):
     with st.sidebar:
         # number of recommendations
         N = st.number_input('Enter number of recommendations', min_value=1, max_value=10)
 
         # movie name
         movie_name = st.selectbox('Enter movie name', options=movie_names['title'])
-        item_id = movie_names.loc[movie_names.loc[movie_names['title'] == movie_name, 'movieId'].index[0], 'movieId']
+        item_id    = movie_names.loc[movie_names.loc[movie_names['title'] == movie_name, 'movieId'].index[0], 'movieId']
 
         # user id
-        user_id = st.selectbox('Enter user ID', options=user_ids)
+        user_name = st.selectbox('Enter user name', options=names['name'])
+        user_id   = names.loc[names.loc[names['name'] == user_name, 'userId'].index[0], 'userId']
 
     return N, movie_name, item_id, user_id
